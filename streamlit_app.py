fae9081acb0e7f79ba81d3a7830e70b69d94b87d
Wassim Jabi <jabiw@cardiff.ac.uk>
2022-09-29 11:50:49 +0100

    Update file

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,14 @@
 import streamlit as st
 
-st.title('🎈 App Name')
-
-st.write('Hello world!')
+st.title('🎈 TopologicPy is on PyPI!')
 
 from topologicpy import topologic
 
+st.write("v1 = topologic.Vertex.ByCoordinates(0,0,0)")
+st.write("v2 = topologic.Vertex.ByCoordinates(10,10,10)")
+st.write("e1 = topologic.Edge.ByStartVertexEndVertex(v1, v2)")
+st.write("c1 = e1.Centroid()")
+st.write("st.write(c1.X(), c1.Y(), c1.Z())")
 v1 = topologic.Vertex.ByCoordinates(0,0,0)
 v2 = topologic.Vertex.ByCoordinates(10,10,10)
 e1 = topologic.Edge.ByStartVertexEndVertex(v1, v2)
